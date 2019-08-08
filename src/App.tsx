f0950753579088dbd86f740fdb5b8202e330c61e
ohseunghyeon <postercolo@gmail.com>
2019-08-08 10:17:10 +0900

    Add Router for month and week view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import Calendar from './components/Calendar';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import MonthView from './components/MonthView';
 
-const App: React.FC = () => {
-  return <Calendar />;
-};
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/calendar/month/:year/:month/:date" component={MonthView} />
+      <Route exact path="/calendar/week/:year/:month/:date" component={MonthView} />
+    </Switch>
+    <Route exact path="/" render={redirectToMonth} />
+  </BrowserRouter>
+);
+
+const redirectToMonth = () => <Redirect to="/calendar/month" />;
 
 export default App;
