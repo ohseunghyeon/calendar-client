dcb3a4d360bc979e11e6b7564e32dcb8b772156c
ohseunghyeon <postercolo@gmail.com>
2019-08-12 15:44:24 +0900

    Update baseURL

--- src/services/fetch.service.ts
+++ src/services/fetch.service.ts
@@ -10,12 +10,10 @@ interface Props {
   };
 }
 
-const baseUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost:8098';
-
 export default {
   fetch: ({ method, body, callback, qs }: Props) => {
     const queries = qs ? `${querystring.stringify(qs)}` : '';
-    fetch(`${baseUrl}/events?${queries}`, {
+    fetch(`${process.env.REACT_APP_SERVER_URL}/events?${queries}`, {
       method,
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
